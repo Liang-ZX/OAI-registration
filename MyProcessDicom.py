0d49a03abe2f5ace211b87923e9b004c0eb7d69a
Liang-ZX <3170105151@zju.edu.cn>
2020-08-22 22:00:38 +0800

    adjust affine transform

--- MyProcessDicom.py
+++ MyProcessDicom.py
@@ -9,6 +9,7 @@ from IPython.display import clear_output
 import math
 import sys
 import AssistVisualization as vis
+from skimage.measure import label
 
 from ipywidgets import interact, fixed
 
@@ -53,12 +54,10 @@ def adaptive_thresh(images):
     for z in range(images.GetDepth()):
         img = sitk.GetArrayFromImage(images[:,:,z])
         img = img.astype(np.uint8)
-#         th1, binary1 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-#         img = cv2.medianBlur(img,5)
-#         binary1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        img = cv2.GaussianBlur(img,(5,5),0)
-        canny = cv2.Canny(img, 50, 80, apertureSize = 3) # threshold2越大，提取的边缘越少
-        dst = canny
+        binary1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+#         img = cv2.GaussianBlur(img,(5,5),0)
+#         canny = cv2.Canny(img, 50, 80, apertureSize = 3) # threshold2越大，提取的边缘越少
+        dst = binary1
         size = 2
         kernel = np.ones((size, size), dtype=np.uint8)
         img_open = cv2.erode(cv2.dilate(dst, kernel), kernel)
@@ -72,9 +71,10 @@ def do_thresh(images):
     for z in range(images.GetDepth()):
         img = sitk.GetArrayFromImage(images[:,:,z])
         img = img.astype(np.uint8)
+        
+        img = cv2.medianBlur(img,3)
+        img = cv2.GaussianBlur(img,(3,3),0)
         th1, binary1 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-#         img = cv2.medianBlur(img,5)
-#         binary1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
         new_images.append(binary1)
     img_array = np.array(new_images)
     return sitk.GetImageFromArray(img_array)
@@ -104,9 +104,25 @@ def postprocess(images):
     img_array = np.array(new_images)
     return sitk.GetImageFromArray(img_array)
 
-
+def get_component_process(images):
+    new_images = []
+    for z in range(images.GetDepth()):
+        img = sitk.GetArrayFromImage(images[:,:,z])
+        img = img.astype(np.uint8)
+        kernel = np.ones((2, 2), dtype=np.uint8)
+        tmp_img = cv2.erode(cv2.dilate(img, kernel), kernel)
+#         contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        lcc = largestConnectComponent(tmp_img)
+#         tmp = np.zeros((384,384))
+#         cv2.drawContours(tmp,contours,-1,(255,255,255),-1) 
+        new_images.append(lcc)
+    img_array = np.array(new_images)
+    return sitk.GetImageFromArray(img_array)
+    
+    
 def preprocess(images):
     tmp_img = do_thresh(images)
+    tmp_img = adaptive_thresh(tmp_img)
     return tmp_img
 
 
@@ -148,6 +164,19 @@ def transform_img(fixed_image, moving_image, transform):
     resample.SetInterpolator(sitk.sitkBSpline)  
     resample.SetTransform(transform)
     return resample.Execute(moving_image)
+
+
+def largestConnectComponent(bw_img):
+    labeled_img, num = label(bw_img, connectivity=1, background=0, return_num=True)    
+    max_label = 0
+    max_num = 0
+    for i in range(1, num+1): # 这里从1开始，防止将背景设置为最大连通域
+        if np.sum(labeled_img == i) > max_num:
+            max_num = np.sum(labeled_img == i)
+            max_label = i
+    lcc = (labeled_img == max_label)
+
+    return lcc.astype(np.uint8)
 
 
 def show_images(fixed_image, moving_image):
